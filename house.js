06752132a44e6293867f5a93e547852c6132455e
Kun Anggiar Lanang <email": null>
2016-09-05 13:51:19 +0700

    shock

--- house.js
+++ house.js
@@ -2,32 +2,47 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(component) {
+    this._address = component ['address']
+    this._square_feet = component ['square_feet']
+    this._num_bedrooms = component ['num_bedrooms'] || 3
+    this._num_baths = component ['num_baths'] || 2
+    this._cost = component ['cost'] || 320000
+    this._down_payment = component ['down_payment'] || 0.20
+    this._sold = component ['sold'] || false
+    this._short_sale = component ['short_sale']
+    this._has_tenants = component ['has_tenants'] || false
   }
 
+
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    let secret = this._address.replace(/.{10}/g, '**********')
+    return `${secret} : ${this._square_feet} sq. ft, ${this._num_bedrooms} bed, ${this._num_baths} bath. $${this._cost}`
   }
 
   buy(money, good_credit) {
-    if (money >= down_payment && good_credit)
-      this.sold = true
+    if (money >= money * this._cost || good_credit * 12 > this._cost)
+      {this._sold = true}
+      else {this._sold = false}
+
+      return `Total saldo = ${money}, kredit perbulan = ${good_credit}, apakah mampu membayar? ${this._sold}`
   }
 
   down_payment() {
-    return cost * this.down_payment
+    let dope = this._cost*this._down_payment
+    return `Harga ${this._cost}, minimal dope = ${dope}`
   }
 
   to_s() {
-    return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
+    return `${this._address} : ${this._square_feet} sq. ft., ${this._num_bedrooms} bed, ${this._num_baths} bath. ${this._cost}`
   }
 }
+let tambun = new House({address: 'Metland_tambun', square_feet : 120, short_sale:true})
+// console.log(tambun.hasOwnProperty('down_payment'))
+console.log(tambun.down_payment())
+console.log(tambun.to_s())
+console.log(tambun.obscure_address())
+console.log(tambun.buy(5000, 700))
+console.log(tambun.buy(900, 2000))
+console.log(tambun.buy(150000, 90000))
+// console.log(tambun.buy(60000, 45000))
