6eb3c420dceee590b37295e2a3d4c1ce29ae59b1
Aji Lantang Mardika & Sahbana <pair+ajilantang.sahbanagold@hacktiv8.com>
2016-09-05 12:57:35 +0700

    aji-sahbana

--- house.js
+++ house.js
@@ -2,58 +2,19 @@
 
 class House {
 
-  constructor(address, num_bedrooms, cost) {
-    this.address = address
-    this.num_bedrooms = num_bedrooms || 3
-    this.cost = cost || 320000
-  }
+  constructor(attributes) {
+    this.owner = attributes['owner']
+    this.address = attributes['address']
+    this.square_feet = attributes['square_feet']
+    this.num_bedrooms = attributes['num_bedrooms'] || 3
+    this.num_baths = attributes['num_baths'] || 2
+    this.cost = attributes['cost'] || 320000
+    this.down_payment = attributes['down_payment'] || 0.20
+    this.sold = attributes['sold'] || false
+    this.short_sale = attributes['short_sale']
+    this.has_tenants = attributes['has_tenants'] || false
 
-  set_square_feet(square_feet) {
-    this.square_feet = square_feet
   }
-
-  set_num_baths(num_baths) {
-    this.num_baths = num_baths || 2
-  }
-
-  set_down_payment(down_payment) {
-    this.down_payment = down_payment || 0.20
-  }
-
-  set_sold(sold) {
-    this.sold = sold || false
-  }
-
-  set_has_tenants(has_tenants) {
-    this.has_tenants = has_tenants || false
-  }
-  set_short_sale(short_sale) {
-    this.short_sale = short_sale
-  }
-  get_square_feet() {
-    return this.square_feet
-  }
-
-  get_num_baths() {
-    return this.num_baths
-  }
-
-
-  get_down_payment() {
-    return this.down_payment()
-  }
-
-  get_sold() {
-    return this.sold
-  }
-
-  get_has_tenants() {
-    return this.has_tenants
-  }
-  get_short_sale() {
-    return this.short_sale
-  }
-
 
   obscure_address() {
     return this.address.replace(/.{10}$/g, '****')
@@ -69,12 +30,19 @@ class House {
   }
 
   to_s() {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}$`
+    return ` Owner: ${this.owner},\n Address: ${this.obscure_address()},\n Area : ${this.square_feet} sq. ft.,\n Bedrooms: ${this.num_bedrooms} bed,\n Bathrooms: ${this.num_baths} bath.\n Cost: ${this.cost}$`
   }
 }
-let azis_house = new House("Jl. Patal Senayan no. 12, Jakarta Selatan", 7, 500000)
-azis_house.set_sold(false)
-azis_house.set_num_baths(3)
-azis_house.set_has_tenants(false)
-azis_house.set_square_feet(false)
+let azis_house = new House({
+  owner: "Sahabanaji",
+  address: "Jl. Patal Senayan no. 12, Jakarta Selatan",
+  square_feet: 10000,
+  num_bedrooms: 7,
+  num_baths: 3,
+  cost: 500000,
+  down_payment: 0.30,
+  sold: false,
+  short_sale: "yes",
+  has_tenants: false
+})
 console.log(azis_house.to_s())
