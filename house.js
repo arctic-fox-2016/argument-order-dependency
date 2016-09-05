8cc9f34fad3693f8bcfb1a638eb9fa4f57f6ccbc
Ahmad Ahmadi & Ryan Andhaka <pair+ahmadahmadi14.ryanandhaka@hacktiv8.com>
2016-09-05 12:24:31 +0700

    argument

--- house.js
+++ house.js
@@ -1,33 +1,54 @@
 "use strict"
-
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+
+  constructor (components) {
+    this.address = components ['address']
+    this.square_feet = components ['square_feet']
+    this.num_bedrooms = components ['num_bedrooms'] || 3
+    this.num_baths = components ['num_baths'] || 2
+    this.cost = components ['cost'] || 320000
+    this.down_payment = components ['down_payment'] || 0.20
+    this.sold = components['sold'] || false
+    this.short_sale = components ['short_sale']
+    this.has_tenants = components ['has_tenants'] || false
+
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
+    // return `${this.address} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
   }
 
   buy(money, good_credit) {
-    if (money >= down_payment && good_credit)
+    if (money >= down_payment || good_credit)
       this.sold = true
   }
 
-  down_payment() {
-    return cost * this.down_payment
+  short_sale(sale, paid){
+    if (sale >= paid)
+    this.short_sale = false
   }
 
+  down_payment() {
+    let dp = this.cost * this.down_payment
+     //return `Harga ${this._cost} , minimal DP = ${dp}`
+
+  }
+
+
   to_s() {
-    return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
+    return `${this.address} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
   }
 }
+
+let rumah = new House({address:'menteng dalam',
+                      square_feet:'1000',
+                      short_sale:'true',
+                      num_baths:'2'})
+
+console.log(rumah.obscure_address());
+
+console.log(rumah.to_s());
+
+//console.log(rumah);
